9d0e51ea91f5b6e923bacd62650717c7a1b31d32
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2020-05-27 08:39:36 +0200

    daily_reset.py war nicht im Repo

---
+++ python_scripts/daily_reset.py
@@ -0,0 +1,5 @@
+hass.states.set("sensor.cee_garage_e", 0)
+hass.states.set("sensor.wohnungsluftung_e", 0)
+hass.states.set("sensor.produktion_e", 0)
+
+logger.info("Tagesnergiezähler auf 0 gestellt")
